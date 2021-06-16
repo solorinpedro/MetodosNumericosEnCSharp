99283e66f074de218e41f0b664e16748a0b91cac
Melvin Ernesto Santana Garcia <melvin_santana@ucne.edu.do>
2021-06-15 23:54:32 -0500

    Agregando Nueva Carpeta.

--- Metodos Numericos En CSharp/Formulas/04 Diferenciación Numérica/Diferenciacion_Numerica.cs
+++ Metodos Numericos En CSharp/Formulas/04 Diferenciación Numérica/Diferenciacion_Numerica.cs
@@ -7,8 +7,10 @@ namespace Metodos_Numericos_En_CSharp
         public void DiferenciacionNumerica()
         {
             Console.WriteLine("-----------------------");
-            int cantidad = 11;//Cantidad de elementos que vamos a tener
-            double h = 0.1;// la distancia o tamaño que tenemos por cada elemento 
+
+            int cantidad = 11;//Cantidad de elementos que vamos a tener.
+
+            double h = 0.1;// la distancia o tamaño que tenemos por cada elemento.
 
             //Aquí Guardamos nuestra tabla
             double[] y = new double[cantidad];
@@ -16,25 +18,27 @@ namespace Metodos_Numericos_En_CSharp
             //Aquí Guardamos la derivada
             double[] derivada = new double[cantidad];
 
-            double paso = 0.1; //Que tanto avanzamos en cada paso al crear la tabla
-            double x = 0; //Que tanto avanzamos cada paso al crear la tabla
+            double paso = 0.1; //Que tanto avanzamos en cada paso al crear la tabla.
+
+            double x = 0; //Que tanto avanzamos cada paso al crear la tabla.
 
             Console.WriteLine("\r\nx\tf(x)");//Encabezado
-            //Creamos la tabla
-            for (int n = 0; n < cantidad; n++)//recorrido de 1 en 1
+
+            //Creamos la tabla.
+            for (int n = 0; n < cantidad; n++)//recorrido de 1 en 1.
             {
                 y[n] = Funcion(x);
                 Console.WriteLine("{0:N1}\t{1:N4}", x, y[n]);
                 x += paso;
             }
 
-            //Calculamos la derivada
+            //Calculamos la derivada.
             for (int n = 1; n < cantidad - 1; n++)
             {
                 derivada[n] = (y[n + 1] - y[n - 1]) / (2 * h);
             }
 
-            //Funciona bien pero no podemos calcular la derivada de 0 y 1
+            //Funciona bien pero no podemos calcular la derivada de 0 y 1.
             Console.WriteLine("\r\nx\tDerivada");
             for (int n = 1; n < cantidad - 1; n++)
             {
