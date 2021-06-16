d7d1e8228bd65a4d34ae0af745a09be07b5764e1
Melvin Ernesto Santana Garcia <melvin_santana@ucne.edu.do>
2021-06-15 23:57:44 -0500

    Agregando Nueva Carpeta.

--- Metodos Numericos En CSharp/Formulas/01 Errores De Precisión/Errores_De_Precision.cs
+++ Metodos Numericos En CSharp/Formulas/01 Errores De Precisión/Errores_De_Precision.cs
@@ -13,7 +13,7 @@ namespace Metodos_Numericos_En_CSharp
             for (int n = 0; n < 10000; n++)
                 sumatoria += 0.1f;
 
-            //No obtenemos 1000
+            //No obtenemos 1000.
             Console.WriteLine("1) Error De Precisión Con Valores Tipo Flotantes");
             Console.WriteLine("El Resultado Es " + sumatoria);
             Console.WriteLine("------------------------");
@@ -23,29 +23,31 @@ namespace Metodos_Numericos_En_CSharp
             for (int n = 0; n < 10000; n++)
                 sumatoriaD += 0.1;
 
-            //Obtenemos 1000 con un pequeño exceso en el valor
+            //Obtenemos 1000 con un pequeño exceso en el valor.
             Console.WriteLine("2) Error De Precisión Con Valores Tipo Dobles");
             Console.WriteLine("El Resultado Es " + sumatoriaD);
             Console.WriteLine("----------------------------------");
 
-            //Este tipo de error se conoce como error de cancelación
-            //pues elimina cifras significativas
+            //Este tipo de error se conoce como error de cancelación.
+
+            //pues elimina cifras significativas.
             float a = 100.45678f;
             float b = 100.45655f;
 
             float c = a - b;
-            //el resultado no es 0.00023
+            //el resultado no es 0.00023.
             Console.WriteLine("3) Error De Precisión Con Valores Tipo Flotantes");
             Console.WriteLine("El Resultado Es " + c);
             Console.WriteLine("-----------------------------");
 
-            //Este tipo de error se conoce como error de cancelación
-            //pues elimina cifras significativas
+            //Este tipo de error se conoce como error de cancelación.
+
+            //pues elimina cifras significativas.
             double aD = 100.45678;
             double bD = 100.45655;
 
             double cD = aD - bD;
-            //el resultado es 0.00023 con un pequeño exceso en el valor
+            //el resultado es 0.00023 con un pequeño exceso en el valor.
             Console.WriteLine("4) Error De Precisión Con Valores Tipo Doble");
             Console.WriteLine("El Resultado Es " + cD);
             Console.WriteLine("--------------------------------------");
@@ -59,7 +61,7 @@ namespace Metodos_Numericos_En_CSharp
             float x2 = (-b2 + (float)Math.Sqrt(descriminante)) / (2 * a1);
 
             /*El error ocurre al restar b del discriminante por eso tenemos 0
-            vemos nuevamente el error  de cancelación*/
+            vemos nuevamente el error  de cancelación.*/
             Console.WriteLine("5) Error De Precisión Con Valores Tipo Flotantes");
             Console.WriteLine("x1={0}, x2={1}, d={2} ", x1, x2, descriminante);
             Console.WriteLine("-------------------------");
@@ -89,7 +91,7 @@ namespace Metodos_Numericos_En_CSharp
             Console.WriteLine("---------------------------------------------");
 
             /*Lo mejor es que usamos double o decimal
-            Si necesitamos presión infinita lo mejor es usar Maple Mathematica*/
+            Si necesitamos presión infinita lo mejor es usar Maple Mathematica.*/
         }
     }
 }
